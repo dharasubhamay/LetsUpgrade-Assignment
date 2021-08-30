9600c11b6415e9ca9e09292595854f5e125cbd98
dharasubhamay <82009535+dharasubhamay@users.noreply.github.com>
2021-08-31 01:03:34 +0530

    Add files via upload

---
+++ Node JS/Assignment/Day-2/languageApi.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+//create express object
+const app = express();
+
+//usebility of json text
+app.use(express.json());
+
+//connect the database
+mongoose.connect("mongodb://localhost:27017/languageapi", {useNewUrlParser:true}, () => {
+    console.log("MongoDB is connected");
+})
+
+//make database schema
+const languageSchema = new mongoose.Schema({
+    name : String,
+    founder : String,
+    imageUrl : String
+})
+
+//create model for collection of languages
+const languageModel = new mongoose.model('languages', languageSchema);
+
+//get all languages 
+app.get("/languages", async (req, res) => {
+    let data = await languageModel.find();
+
+    res.send(data);
+})
+
+//get a single language by it's id
+app.get("/language/:id", async (req, res) => {
+    let id = req.params.id;
+    let language = await languageModel.find({_id:id});
+
+    res.send(language);
+})
+
+//find a single language by it's name
+app.get("/language/name/:name", async (req, res) => {
+    let name = req.params.name;
+    let languagedata = await languageModel.find({name:name});
+
+    res.send(languagedata);
+})
+
+//create a language
+app.post("/createlang", (req, res) => {
+    let lang = req.body;
+    console.log(lang);
+
+    let langObj = new languageModel(lang);
+
+    langObj.save((err, data) => {
+        if(err === null)
+        {
+            res.send({message: "Language Created"});
+        }
+    });
+})
+
+//delete a single language by it's id
+app.delete("/deletelang/:id", (req, res) => {
+    let id = req.params.id;
+
+    languageModel.deleteOne({_id:id}, (err, data) => {
+        if(err === null)
+        {
+            res.send({message:"Language deleted"});
+        }
+    });
+})
+
+//update a single language by it's id
+app.put("/updatelang/:id", (req, res) => {
+    let id = req.params.id;
+    let lang = req.body;
+
+    languageModel.updateOne({_id:id}, lang, (err, data) => {
+        if(err === null)
+        {
+            res.send("language updated");
+        }  
+    })
+})
+
+
+app.listen(8000, () => {
+    console.log("Server is running");
+})
+
